6ebfadea4609c91f08a2598a0a16a0306fa8b129
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-03-12 21:22:22 +0000

    ajouter imigration , factory , model de user

---
+++ app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable  implements MustVerifyEmail
+{
+
+    use HasFactory, Notifiable,HasApiTokens;
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'password',
+        'lastLogin',
+        'isActive',
+        'settings',
+        'role',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'lastLogin' => 'datetime',
+        'isActive' => 'boolean',
+        'settings' => 'array',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+
+    }
+
+
+
