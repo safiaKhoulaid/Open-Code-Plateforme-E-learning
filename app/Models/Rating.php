965e17046ce71726556454adaa614c1378c274af
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-04-29 08:48:43 +0000

    Ajout de la fonctionnalité HasFactory dans le modèle Rating : intégration de la fabrique pour faciliter la création d'instances de Rating et ajout de la relation belongsTo pour une meilleure gestion des associations.

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rating extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'user_id',
         'course_id',
